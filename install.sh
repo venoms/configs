c8204a66afa683af1549318861ff94177d22808a
zemnmez <zemnmez@googlemail.com>
2015-09-23 12:18:34 +0100

    better linux support

--- install.sh
+++ install.sh
@@ -11,8 +11,20 @@ sed '/vundle#end/{q;}' $DIR/.vimrc > ~/.vimrc
 
 vim -c 'BundleInstall!' -c q
 
-( cd ~/.vim/bundle/YouCompleteMe && ./install.py --omnisharp-completer --gocode-completer --clang-completer ;)
+params="install.py --omnisharp-completer --gocode-completer --clang-completer"
 
+function installycm {
+	if [[ "$(python -c "import sys;print(sys.version_info[0])")" == "3" ]]; then
+		if ! python2 $params; then
+			echo "can't find python2"
+			exit
+		fi
+	else
+		python $params
+	fi
+}
+
+( cd ~/.vim/bundle/YouCompleteMe ; installycm ;) # it breaks if you don't end with a winky face
 
 rm ~/.vimrc
 $DIR/link.sh
